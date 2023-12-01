967e6d22ce266e40420c903e57a2d69fa1e22a90
adamst27 <144603337+adamst27@users.noreply.github.com>
2023-12-01 20:58:17 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const hiddenElement = document.querySelectorAll(".hidden");
+hiddenElement.forEach((el) => observer.observe(el));
